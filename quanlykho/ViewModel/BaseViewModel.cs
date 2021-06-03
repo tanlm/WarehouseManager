c9741f057fa3ba0e18bdd065ca94aa386200482e
tanlm <tanleminh1311@gmail.com>
2021-06-04 05:27:40 +0700

    commit all source

---
+++ quanlykho/ViewModel/BaseViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace quanlykho.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    class RelayCommand<T> : ICommand
+    {
+        private readonly Predicate<T> _canExcecute;
+        private readonly Action<T> _excecute;
+
+        public RelayCommand(Predicate<T> canExecute, Action<T> excecute)
+        {
+            if (excecute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            _canExcecute = canExecute;
+            _excecute = excecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+
+            try
+            {
+                return _canExcecute == null ? true : _canExcecute((T)parameter);
+            }
+            catch
+            {
+                return true;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _excecute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ quanlykho/ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace quanlykho.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool IsLoaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+        public ICommand UnitCommand { get; set; }
+
+        public ICommand SuplierCommand { get; set; }
+        // moi thu xu ly se nam trong nay
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                IsLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            });
+
+            UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {   
+                UnitWindow unitWindow = new UnitWindow();
+                unitWindow.ShowDialog();
+            });
+
+            SuplierCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                SuplierWindow unitWindow = new SuplierWindow();
+                unitWindow.ShowDialog();
+            });
+        }
+    }
+}
