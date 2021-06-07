c89b49255852ae2d9d795270bda98586cd5e528e
tanlm <tanleminh1311@gmail.com>
2021-06-07 12:16:36 +0700

    add new project

---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,74 @@
+﻿using QuanLyKho.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLyKho.ViewModel
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public bool IsLogin { get; set; }
+        private string _UserName;
+        public string UserName { get => _UserName; set { _UserName = value; OnPropertyChanged(); } }
+
+        private string _PassWord;
+        public string PassWord { get => _PassWord; set { _PassWord = value; OnPropertyChanged(); } }
+        public ICommand LoginCommand { get; set; }
+        public ICommand PasswordChangedCommand { get; set; }
+        public ICommand CloseFormLoginWindow { get; set; }
+
+        // mọi thứ xử lý sẽ nằm trong này
+        public LoginViewModel()
+        {
+            IsLogin = false;
+            LoginCommand = new RelayCommand<Window>((p) => { return true; }, (p) => { Onlogin(p); });
+            PasswordChangedCommand = new RelayCommand<PasswordBox>((p) => { return true; }, (p) => { PassWord = p.Password; });
+            CloseFormLoginWindow = new RelayCommand<Window>((p) => { return true; }, (p) => {IsLogin = false ;p.Close(); });
+        }
+
+        void Onlogin(Window p)
+        {
+            if (p == null)
+            {
+                return;
+            }
+            string passEncode = MD5Hash(Base64Encode(PassWord));
+            var isExistsUser = DataProvider.Ins.DB.Users.Any(x => x.UserName == UserName && x.Password == passEncode);
+            if (isExistsUser)
+            {
+                IsLogin = true;
+                p.Close();
+            }
+            else
+            {
+                IsLogin = false;
+                MessageBox.Show("Sai tài khoản hoặc mật khẩu!");
+            }
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string MD5Hash(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
+        }
+    }
+}
---
+++ ViewModel/UnitViewModel.cs
@@ -0,0 +1,21 @@
+﻿using QuanLyKho.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKho.ViewModel
+{
+    public class UnitViewModel : BaseViewModel
+    {
+        private ObservableCollection<Unit> _UnitList;
+        public ObservableCollection<Unit> UnitList { get => _UnitList; set { _UnitList = value; OnPropertyChanged(); } }
+
+        public UnitViewModel()
+        {
+            UnitList = new ObservableCollection<Unit>(DataProvider.Ins.DB.Units.ToList());
+        }
+    }
+}
